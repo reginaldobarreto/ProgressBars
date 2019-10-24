3b52d1bd8c438fe240366794fce47bf1aaac0cb6
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-10-24 14:48:22 -0300

    configurada ação ao botão

--- app/src/main/java/br/com/barrsoft/progressbars/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/progressbars/MainActivity.java
@@ -3,12 +3,46 @@ package br.com.barrsoft.progressbars;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName() ;
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.circular_progress)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ButterKnife.bind(this);
+        progressBar.setVisibility(View.GONE);
+
     }
+
+    @OnClick(R.id.button)
+    public void submit(View view) {
+        // TODO submit data to server...
+
+        Toast.makeText(getApplicationContext(),"ProgressBar Start",Toast.LENGTH_SHORT).show();
+        progressBarCounter();
+    }
+
+    private void progressBarCounter() {
+
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
 }
